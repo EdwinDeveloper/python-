df988cfcaaf21310d0872ae9a212519e1bd97674
Edwin <edwin_020193@hotmail.com>
2019-01-12 16:02:36 -0600

    primer commit

---
+++ Edwin/Edwin/__init__.py
@@ -0,0 +1,39 @@
+
+
+v=12
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+nombre="Edwin"
+apellido="Perez"
+print(nombre+" : "+str(12))
+list = (1,2,3,4,5,6,6,"Memo")
+lista = [8,7,6,5,4,3,3,3,"Edwin","Daniel"]
+for data in lista:
+    print(data)
+    pass
+
+while 1==2:
+    print("Si inge si")
+    pass
+
+if 5>1:
+  print("Five is greater than two!")
+  pass
+
+print("Enter your name")
+"""x = input()
+print("Your name is : "+x)"""
+
+Edwin = {
+    "nombre":"Edwin",
+    "edad":21
+}
+
+print(Edwin["nombre"])
+
+def hola(nombre):
+    print("hola"+nombre)
+
+hola("Si inge si")
